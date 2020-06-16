40ea30a7ace3f2cda4d6726ef7bdb40d07c5c3eb
U Ahmad <UmadAhmad1928@gmail.com>
2020-06-16 17:12:27 +0500

    Changed images from .jpeg to .png

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,14 +33,12 @@ const upload = multer({
 exports.resizeAvatar = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
-  // req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-  req.file.filename = `user-${req.user.id}-${Date.now()}.png`;
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
   sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
-    .png({ quality: 90 })
-    // .jpeg({ quality: 90 })
+    .jpeg({ quality: 90 })
     .toFile(`public/images/users/${req.file.filename}`);
 
   next();
